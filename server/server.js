b1b0c8d986ba1464319bf04902dc19eee65d86ca
gardemanuelsen <74247742+gardemanuelsen@users.noreply.github.com>
2022-03-01 13:57:49 +0100

    use port from enviroment

--- server/server.js
+++ server/server.js
@@ -11,5 +11,5 @@ app.post("/login",(req,res,next)=>{
 })
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log("Started on http://localhost:"${server.adress().port})
+    console.log(`Started on http://localhost:${server.address().port}`);
 });
